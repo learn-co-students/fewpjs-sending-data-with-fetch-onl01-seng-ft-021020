9e95229fe2462b998c1020335fda285e95cb1ef5
mattted <medward4@gmail.com>
2020-05-14 11:27:27 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  let postObj = {
+    name: userName,
+    email: userEmail
+  };
+
+  let url = 'http://localhost:3000/users';
+  let postConfig = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(postObj)
+  };
+
+  return fetch(url, postConfig)
+    .then( (response) => response.json())
+    .then( (data) => {
+      let newId = document.createElement('p')
+      newId.textContent = data.id 
+      document.querySelector('body').appendChild(newId)
+    })
+    .catch( (error) => {
+      let newError = document.createElement('p')
+      newError.textContent = error.message
+      document.querySelector('body').appendChild(newError)
+    })
+}
